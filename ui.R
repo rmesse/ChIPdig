e4f142fdb56ca165c540683dc5ec7958d6a18ad2
rmesse <30732844+rmesse@users.noreply.github.com>
2018-05-22 17:47:22 -0400

    Update ui.R

--- ui.R
+++ ui.R
@@ -92,17 +92,13 @@ shinyUI(pageWithSidebar(
   
   mainPanel(
     tabsetPanel(id = "inTabset",
-      tabPanel(title = "Mapping", value = "map", uiOutput("text1"),  uiOutput('text2'), tableOutput('singleread_exp_input_example'), uiOutput('text3'),   tableOutput('pairedend_exp_input_example'),uiOutput('text4'),uiOutput("fastq_list_title"), tableOutput('fastq_list'),uiOutput('text5'),uiOutput("mapping_success")),
+      tabPanel(title = "Mapping", value = "map", span(h3(textOutput("warning_mapping")) , style="color:red"),uiOutput("text1"),  uiOutput('text2'), tableOutput('singleread_exp_input_example'), uiOutput('text3'),    tableOutput('pairedend_exp_input_example'),uiOutput('text4'),uiOutput("fastq_list_title"), tableOutput('fastq_list'),uiOutput('text5'),uiOutput("mapping_success")),
       tabPanel(title = "Initial processing of mapped reads", value = "initial_proc", uiOutput('text8'), tableOutput('data_frame_processing_example'),uiOutput("sample_list_title"),   span(h3(textOutput("warning1")) , style="color:red"),uiOutput('warning2'), tableOutput('warning3') ,   tableOutput('sample_list'),uiOutput("sample_list_with_parameters_title"),tableOutput('sample_list_with_parameters'),uiOutput("chrom_sizes_title"),tableOutput('chrom_sizes'),uiOutput("MDS_all_title"),plotOutput('MDS_all', width = "65%"),downloadButton('MDS_all_download', 'Download plot')),
       tabPanel(title = "Peak calling", value = "peak_calling", uiOutput("table_with_peaks_title"),tableOutput('table_with_peaks'),uiOutput("ext_peaks_table_title"),tableOutput('ext_peaks_table')),
       tabPanel(title = "Differential enrichment analysis", value = "diff",h3("Mean-difference plot:"),plotOutput('results_plot', width = "65%"),downloadButton('results_plot_download', 'Download plot'),h3("Box-and-whisker plot:"),   plotOutput('boxplot_Q95', width = "50%"),downloadButton('boxplot_Q95_download', 'Download plot')    ,h3("Results table:"),downloadButton('results_table_download', 'Download table'),tableOutput('results')),
       tabPanel(title = "Peak annotation", value = "anno", plotOutput('annotated_pie', width = "50%"),downloadButton('annotated_pie_download', 'Download plot'), tableOutput('annotated_table'),downloadButton('annotated_table_download', 'Download table')),
-      tabPanel(title = "Heatmaps and metaplots", value = "heatmaps", uiOutput('text6'),tableOutput('bedgraph_tab_example'), tableOutput('sample_list_heatmaps'), h3("Heatmaps:"),plotOutput('heatmaps_heatmaps'),uiOutput("width_heatmaps"),uiOutput("height_heatmaps"),downloadButton('heatmaps_heatmaps_download', 'Download plot'),h3("Metaplot:"),plotOutput('heatmaps_metaplot',width = "65%"),uiOutput("width_metaplot"),uiOutput("height_metaplot"),downloadButton('heatmaps_metaplot_download', 'Download plot'))
+      tabPanel(title = "Heatmaps and metaplots", value = "heatmaps", span(h3(textOutput("warning1_h")) , style="color:red") ,uiOutput('text6'),tableOutput('bedgraph_tab_example'), tableOutput('sample_list_heatmaps'), h3("Heatmaps:"),plotOutput('heatmaps_heatmaps'),uiOutput("width_heatmaps"),uiOutput("height_heatmaps"),downloadButton('heatmaps_heatmaps_download', 'Download plot'),h3("Metaplot:"),plotOutput('heatmaps_metaplot',width = "65%"),uiOutput("width_metaplot"),uiOutput("height_metaplot"),downloadButton('heatmaps_metaplot_download', 'Download plot'))
    ))    
   
 ))
 
-# bookmarking
-# broad peaks option - merge peaks spacing less than X
-# try to remove GR objects upon peak calling
-# test if significance without peak intersection works
